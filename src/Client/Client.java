9e5a74ffeb093c155ee075affbda9c1f88f09c37
ZviMints <ZviMints@gmail.com>
2018-11-13 12:26:47 +0200

    Added GUI

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -5,8 +5,10 @@
 package Client;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -15,6 +17,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import Server.ThreadSERVER;
 
 public class Client {
 	private static final String host = "localhost";
@@ -43,11 +47,11 @@ public class Client {
 			Thread serverAccessThread = new Thread(threadCLIENT); // Open new Thread for each user
 			serverAccessThread.start();
 
-		    PrintStream output = new PrintStream(skt.getOutputStream());
-		    // send UserName to server
-		    output.println(username);
-	
-		  
+			PrintStream output = new PrintStream(skt.getOutputStream());
+			// send UserName to server
+			output.println(username);
+
+
 		}
 		catch(Exception e)
 		{
@@ -55,16 +59,30 @@ public class Client {
 		}
 	}
 
+	/* ************************** InitWindow Setters and Getters ************************** */
+	private static String getNameFromForm()
+	{
+		return txtEnterNameHere.getText();
+	}
+	private static boolean Closed()
+	{
+		return  !login_frame.isVisible();
+	}
+	
 	/* ************************** InitWindow ************************** */
+	static JTextField txtEnterNameHere;
+	static JFrame login_frame;
+	static boolean AlredyInUse = false;
 	public static void InitWindow()
 	{
-		JFrame login_frame = new JFrame();
+		login_frame = new JFrame();
 		login_frame.setTitle("T&O Chat: Login\r\n");
 		login_frame.setBounds(100, 100, 522, 188);
 		login_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		login_frame.getContentPane().setLayout(null);
 
-		JTextField txtEnterNameHere = new JTextField();
+		txtEnterNameHere = new JTextField();
+		txtEnterNameHere.setText("");
 		txtEnterNameHere.setBounds(230, 67, 254, 49);
 		login_frame.getContentPane().add(txtEnterNameHere);
 		txtEnterNameHere.setColumns(10);
@@ -79,40 +97,40 @@ public class Client {
 		login_button.setBounds(20, 60, 204, 69);
 		login_frame.setVisible(true);
 
-		// On Click "Login"
 		login_button.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				if(txtEnterNameHere.getText().contains(" ")) // remove whitespaces
 				{
-					JOptionPane.showMessageDialog(null, "Invalid name! \n the name contain spaces or \n"
-							+"alredy in use"); 
-					/// NEED TO ADD SEARCH FOR USED
+					JOptionPane.showMessageDialog(null, "Invalid name! \n "
+							+ "the name contain spaces"); 
 				}
+//				else if(AlredyInUse(txtEnterNameHere.getText())) // if name alredy in use
+//				{
+//					JOptionPane.showMessageDialog(null, "Invalid name! \n "
+//							+ "the name alredy in use"); 
+//				}
 				else
 				{
-					// NEED TO SEND txtEnterNameHere.getText() and CLOSE .
+					login_frame.setVisible(false);
+					login_frame.dispose();
 				}
 			}
 		});
 	}
-	
+
 	/* ************************** Main ************************** */
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		InitWindow();
-		String get_username = null; 
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter username:");
-		while(get_username == null || get_username.trim().equals(""))
+		while(true)
 		{
-			get_username = scan.nextLine();
-			if(get_username.trim().equals("")) // remove whitespaces
+			Thread.sleep(500);
+			if(Closed())
 			{
-				System.out.println("Invalid username, try again");
+				Client client = new Client(getNameFromForm(),host,PORT);
+				client.startClient();
+				break;
 			}
 		}
-		// Now there a user with name == get_username
-		Client client = new Client(get_username,host,PORT);
-		client.startClient();
 	}
 }
